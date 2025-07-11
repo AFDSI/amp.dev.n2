40aa8a753ced77f2820fcaef5c641c29105e02a7
jay gray <jaygray0919@gmail.com>
2025-07-11 07:29:51 -0500

    Fix: lint.js

--- gulpfile.js/lint.js
+++ gulpfile.js/lint.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS-IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
 const gulp = require('gulp');
@@ -26,7 +10,11 @@ const growReferenceChecker = require('@lib/tools/growReferenceChecker');
 const log = require('@lib/utils/log')('Linter');
 
 function lintNode() {
-  return sh('npm run lint:node');
+  // Use npx eslint directly to avoid issues with npm in sub-processes
+  return sh('npx eslint "**/*.js"', {
+    // Changed to npx eslint and added env
+    env: process.env, // Ensure environment variables are inherited
+  });
 }
 
 function lintYaml() {
